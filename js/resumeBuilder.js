580297c0ac1eb904d3de54b0ed0c7f5593db7971
Yusuf Yalım <yusufyalim@gmail.com>
2016-12-18 15:07:11 +0300

    Beatify JS

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -37,38 +37,32 @@ var bio = {
 };
 
 var education = {
-  schools: [
-    {
-      name: 'Marmara University',
-      location: 'Istanbul / Turkey',
-      degree: 'Bachelor\'s degree',
-      majors: ['Teacher Training in Computer and Control'],
-      dates: '2010 - 2015',
-      url: 'http://elkbil.tef.marmara.edu.tr/en/'
-    },
-    {
-      name: 'Balıkesir University',
-      location: 'Balıkesir / Turkey',
-      degree: 'Associate\'s degree',
-      majors: ['computer Programming'],
-      dates: '2007 - 2009',
-      url: 'http://www.balikesir.edu.tr/index.php/baun/birim/edremit_meslek_yuksekokulu'
-    }
-  ],
-  onlineCourses: [
-    {
-      title: 'Ruby on Rails: An Introduction',
-      school: 'Johns Hopkins University on Coursera',
-      dates: '2015',
-      url: 'https://www.coursera.org/account/accomplishments/verify/N6X3QKXKHGAC'
-    },
-    {
-      title: 'Rails with Active Record and Action Pack',
-      school: 'Johns Hopkins University on Coursera',
-      dates: '2015',
-      url: 'https://www.coursera.org/account/accomplishments/verify/TQF329BCKXFH'
-    }
-  ],
+  schools: [{
+    name: 'Marmara University',
+    location: 'Istanbul / Turkey',
+    degree: 'Bachelor\'s degree',
+    majors: ['Teacher Training in Computer and Control'],
+    dates: '2010 - 2015',
+    url: 'http://elkbil.tef.marmara.edu.tr/en/'
+  }, {
+    name: 'Balıkesir University',
+    location: 'Balıkesir / Turkey',
+    degree: 'Associate\'s degree',
+    majors: ['computer Programming'],
+    dates: '2007 - 2009',
+    url: 'http://www.balikesir.edu.tr/index.php/baun/birim/edremit_meslek_yuksekokulu'
+  }],
+  onlineCourses: [{
+    title: 'Ruby on Rails: An Introduction',
+    school: 'Johns Hopkins University on Coursera',
+    dates: '2015',
+    url: 'https://www.coursera.org/account/accomplishments/verify/N6X3QKXKHGAC'
+  }, {
+    title: 'Rails with Active Record and Action Pack',
+    school: 'Johns Hopkins University on Coursera',
+    dates: '2015',
+    url: 'https://www.coursera.org/account/accomplishments/verify/TQF329BCKXFH'
+  }],
   display: function() {
     this.schools.forEach(function(school) {
       $('#education').append(HTMLschoolStart);
@@ -96,36 +90,31 @@ var education = {
 };
 
 var work = {
-  jobs: [
-    {
-      employer: 'Momentic',
-      title: 'Web Developer',
-      location: 'İstanbul / Turkey',
-      dates: 'October 2016 - in progress',
-      description: 'I am working as Momentic\' Team member to build web application of momentic which is great way to tell stories with pictures'
-    },
-    {
-      employer: 'İdemama',
-      title: 'Web Developer',
-      location: 'Istanbul / Turkey',
-      dates: 'December 2015 - September 2016  ',
-      description: 'I was recruited to develop new features for idemama.com such as idemama market which freelance logo designers can offer to sale their designs on our website.'
-    },
-    {
-      employer: 'Nomad Commerce',
-      title: 'Web Developer',
-      location: 'Istanbul / Turkey',
-      dates: 'February 2015 - December 2016  ',
-      description: 'I was there to continue development of the forivia.com(project moved to another platform after I left the company) later on I build and developed other projects such us backend services for Devrim Erbil\'s exhibits. In nomad commerce I\'ve worked with a large team and various technologies. It was a good experience to learn.'
-    },
-    {
-      employer: 'Bilende',
-      title: 'Web Developer Intern',
-      location: 'Istanbul / Turkey',
-      dates: 'July 2012 - September 2014',
-      description: 'I joined Bilende team as a intern and still my education in university continues. Nevertheless I took responsibilities to maintain existing projects and begin to develop my first applications there. It was a great beginning to my web development career and I am proud being a part of Bilende team.'
-    }
-  ],
+  jobs: [{
+    employer: 'Momentic',
+    title: 'Web Developer',
+    location: 'İstanbul / Turkey',
+    dates: 'October 2016 - in progress',
+    description: 'I am working as Momentic\' Team member to build web application of momentic which is great way to tell stories with pictures'
+  }, {
+    employer: 'İdemama',
+    title: 'Web Developer',
+    location: 'Istanbul / Turkey',
+    dates: 'December 2015 - September 2016  ',
+    description: 'I was recruited to develop new features for idemama.com such as idemama market which freelance logo designers can offer to sale their designs on our website.'
+  }, {
+    employer: 'Nomad Commerce',
+    title: 'Web Developer',
+    location: 'Istanbul / Turkey',
+    dates: 'February 2015 - December 2016  ',
+    description: 'I was there to continue development of the forivia.com(project moved to another platform after I left the company) later on I build and developed other projects such us backend services for Devrim Erbil\'s exhibits. In nomad commerce I\'ve worked with a large team and various technologies. It was a good experience to learn.'
+  }, {
+    employer: 'Bilende',
+    title: 'Web Developer Intern',
+    location: 'Istanbul / Turkey',
+    dates: 'July 2012 - September 2014',
+    description: 'I joined Bilende team as a intern and still my education in university continues. Nevertheless I took responsibilities to maintain existing projects and begin to develop my first applications there. It was a great beginning to my web development career and I am proud being a part of Bilende team.'
+  }],
   display: function() {
     this.jobs.forEach(function(job) {
       $('#workExperience').append(HTMLworkStart);
@@ -140,20 +129,17 @@ var work = {
 };
 
 var projects = {
-  projects: [
-    {
-      title: 'İdemama Market',
-      dates: 'December 2015 - June 2016',
-      description: 'A market which you can buy logos from professional designers.',
-      images: ['images/idemama.png']
-    },
-    {
-      title: 'Imaginable Guidelines İstanbul',
-      dates: 'June 2014 - Decembe 2014',
-      description: 'A platform which you can talk about İstanbul and upload pictures from its streets.',
-      images: ['images/igi.png']
-    }
-  ],
+  projects: [{
+    title: 'İdemama Market',
+    dates: 'December 2015 - June 2016',
+    description: 'A market which you can buy logos from professional designers.',
+    images: ['images/idemama.png']
+  }, {
+    title: 'Imaginable Guidelines İstanbul',
+    dates: 'June 2014 - Decembe 2014',
+    description: 'A platform which you can talk about İstanbul and upload pictures from its streets.',
+    images: ['images/igi.png']
+  }],
   display: function() {
     this.projects.forEach(function(project) {
       $('#projects').append(HTMLprojectStart);
